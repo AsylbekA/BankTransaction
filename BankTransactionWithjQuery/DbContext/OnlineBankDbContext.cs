3ebd2eba03b81199d540cf7a836125ab2ec01cfb
AsylbekA <@anarbayasylbek@gmail.com>
2020-08-01 12:04:20 +0600

    Add project files.

---
+++ BankTransactionWithjQuery/DbContext/OnlineBankDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BankTransactionWithjQuery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactionWithjQuery.DbContext
+{
+    public class OnlineBankDbContext:Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<TransactionModel> Transactions { get; set; }
+
+        public OnlineBankDbContext(DbContextOptions<OnlineBankDbContext> options)
+            :base(options)
+        {}
+    }
+}
